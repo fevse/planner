97a73a83d248941b9ed0ecd1b387be78f7a62cbe
fevse <fedulov.v.s@gmail.com>
2023-05-16 18:27:49 +0600

    incorporate a SQL database

--- models/events.py
+++ models/events.py
@@ -1,15 +1,34 @@
-from pydantic import BaseModel
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-from typing import List
 
-
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
+
+    class Config:
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "title": "Event title",
+                "image": "https://forexample.com/image.png",
+                "description": "Brief description of this event",
+                "tags": ["python", "fastapi", "database", "tests"],
+                "location": "Planet Earth"
+            }
+        }
+
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
 
     class Config:
         schema_extra = {
