7b3c28b518ac3eb1efffbf1089e640036828b0cf
fevse <fedulov.v.s@gmail.com>
2023-05-22 14:49:58 +0600

    fixed bugs

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -36,7 +36,7 @@ def verify_access_token(token: str):
             )
         return data
 
-    except jwt.JWTError:
+    except jwt.PyJWTError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid token"
